408afbcacddb3329b0d1c67f816aaff261504e60
Bo Granlund <bo.granlund@helsinki.fi>
2022-03-12 21:10:14 +0200

    Separate tests and program logic from eachother

--- src/TODO.js
+++ src/TODO.js
@@ -1,4 +1,43 @@
-function kala() {
-    return 242;
+function yourFunction(rec) {
+    let rv = [];
+    var foo = rec.fields;
+    foo.forEach(function(entry) {
+        //console.log(entry);
+    });
+
+    //console.log("foo: " + rec.fields);
+    for(const element of foo) {
+        //console.log("element -> " + element["tag"]);
+        let subf = element["subfields"];
+        if (subf === undefined) {
+            //console.log("subf kalaaa");
+            continue;
+        }
+        for(const e2 of subf) {
+            //console.log("pushin " + e2["value"]);
+            rv.push(e2["code"]+":"+e2["value"]);
+        }
+    }
+    return rv;
 }
-module.export = [kala];
+
+function yourFunction2(rec) {
+    let rv = [];
+    var f = rec.fields;
+    //f.forEach(function (entry) {
+    //    console.log("kakkonen: " + entry);
+    //});
+    
+    for (const e1 of f) {
+        let subf = e1["subfields"];
+        if (subf === undefined) {
+            continue;
+        }
+        for (const e2 of subf) {
+            rv.push(e2["code"]+";"+e2["value"]);
+        }
+    }
+    return rv;
+}
+
+module.exports = { yourFunction, yourFunction2 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,49 +1,7 @@
 import {expect} from 'chai';
 import fixturesFactory, {READERS} from '@natlibfi/fixura';
 import {MarcRecord} from '@natlibfi/marc-record';
-import TODO from './TODO';
-
-function yourFunction(rec) {
-    let rv = [];
-    var foo = rec.fields;
-    foo.forEach(function(entry) {
-        //console.log(entry);
-    });
-
-    //console.log("foo: " + rec.fields);
-    for(const element of foo) {
-        //console.log("element -> " + element["tag"]);
-        let subf = element["subfields"];
-        if (subf === undefined) {
-            //console.log("subf kalaaa");
-            continue;
-        }
-        for(const e2 of subf) {
-            //console.log("pushin " + e2["value"]);
-            rv.push(e2["code"]+":"+e2["value"]);
-        }
-    }
-    return rv;
-}
-
-function yourFunction2(rec) {
-    let rv = [];
-    var f = rec.fields;
-    //f.forEach(function (entry) {
-    //    console.log("kakkonen: " + entry);
-    //});
-    
-    for (const e1 of f) {
-        let subf = e1["subfields"];
-        if (subf === undefined) {
-            continue;
-        }
-        for (const e2 of subf) {
-            rv.push(e2["code"]+";"+e2["value"]);
-        }
-    }
-    return rv;
-}
+import {yourFunction, yourFunction2} from './TODO';
 
 describe('index', () => {
   const {getFixture} = fixturesFactory({
