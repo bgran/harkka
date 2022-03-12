1ffb9cd05a2c339276a77e7fe7291547daa958ce
Bo Granlund <bo.granlund@helsinki.fi>
2022-03-12 19:48:04 +0200

    Implemented second test case

--- src/index.spec.js
+++ src/index.spec.js
@@ -7,20 +7,39 @@ function yourFunction(rec) {
     let rv = [];
     var foo = rec.fields;
     foo.forEach(function(entry) {
-        console.log(entry);
+        //console.log(entry);
     });
 
-    console.log("foo: " + rec.fields);
+    //console.log("foo: " + rec.fields);
     for(const element of foo) {
-        console.log("element -> " + element["tag"]);
+        //console.log("element -> " + element["tag"]);
         let subf = element["subfields"];
         if (subf === undefined) {
-            console.log("subf kalaaa");
+            //console.log("subf kalaaa");
             continue;
         }
         for(const e2 of subf) {
-            console.log("pushin " + e2["value"]);
+            //console.log("pushin " + e2["value"]);
             rv.push(e2["code"]+":"+e2["value"]);
+        }
+    }
+    return rv;
+}
+
+function yourFunction2(rec) {
+    let rv = [];
+    var f = rec.fields;
+    //f.forEach(function (entry) {
+    //    console.log("kakkonen: " + entry);
+    //});
+    
+    for (const e1 of f) {
+        let subf = e1["subfields"];
+        if (subf === undefined) {
+            continue;
+        }
+        for (const e2 of subf) {
+            rv.push(e2["code"]+";"+e2["value"]);
         }
     }
     return rv;
@@ -46,7 +65,7 @@ describe('index', () => {
         const recordData = getFixture('02', 'record.json');
         const record = new MarcRecord(recordData);
         const expectedResults = getFixture('02', 'expectedResults.json');
-        const result = yourFunction2(rec);
+        const result = yourFunction2(record);
 
         expect(result).to.eql(expectedResults);
   });
